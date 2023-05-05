ea7cbcf6d43fdf4cd1f6e53748b6e211b851648c
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-05-06 01:12:01 +0200

    maxsize param implemented

--- query/OsmQuery/OsmQuery.py
+++ query/OsmQuery/OsmQuery.py
@@ -142,7 +142,7 @@ def get_all_combinations(key_values_in):
     return combinations
     
 
-def get_queries_from_tags(config_dir, config_name, out_format, timeout, tags, conditions):
+def get_queries_from_tags(config_dir, config_name, const_params: dict, tags, conditional_params, do_write_const_param_names):
     query_path = os.path.join(config_dir, config_name + ".query")
 
     with open(query_path, "r") as fin:
@@ -154,8 +154,8 @@ def get_queries_from_tags(config_dir, config_name, out_format, timeout, tags, co
     for var in variables:
         possible_values[var] = []
 
-    possible_values["out_format"] = [out_format]
-    possible_values["timeout"] = [timeout]
+    for key, value in const_params.items():
+        possible_values[key] = [value]
 
     for tag, tag_values in tags.items():
         if tag not in possible_values.keys():
@@ -164,9 +164,9 @@ def get_queries_from_tags(config_dir, config_name, out_format, timeout, tags, co
         for tag_value, tag_value_conditions in tag_values.items():
             conditions_met = True
             for condition_name, condition_value in tag_value_conditions.items():
-                if condition_name not in conditions.keys():
+                if condition_name not in conditional_params.keys():
                     continue
-                condition_evaluator = conditions[condition_name]
+                condition_evaluator = conditional_params[condition_name]
                 if not condition_evaluator(condition_value):
                     conditions_met = False
                     break
@@ -181,6 +181,8 @@ def get_queries_from_tags(config_dir, config_name, out_format, timeout, tags, co
         query_name = f"{config_name}-"
         name_parts = []
         for var, value in var_combination.items():
+            if var in const_params and not do_write_const_param_names:
+                continue
             name_parts.append(f"{var}={value}")
         query_name += ",".join(name_parts)
 
@@ -191,31 +193,36 @@ def get_queries_from_tags(config_dir, config_name, out_format, timeout, tags, co
 
 def execute_query(endpoint, query, output_dir, output_ext):
     query_name = query[0]
-    print(f"Executing {query_name}")
+    print(f"Executing {query_name}...")
 
     fname = os.path.join(output_dir, f"{query_name}.{output_ext}")
     if os.path.exists(fname):
-        print(f"\tFile {fname} already exists, skipping query {query_name}")
-        return
+        print(f"\tFile {fname} already exists, skipping.")
+        return True
 
     query_text = query[1]
     resp = requests.post(endpoint, query_text)
     if resp.status_code != 200:
         error_msg = resp.content
         print(f"\tRequest failed for query {query_name}: {error_msg}")
-    else:
-        print(f"\t{query_name} returned code {resp.status_code}")
-        fname = os.path.join(output_dir, f"{query_name}.{output_ext}")
-        with open(fname, "wb") as fout:
-            fout.write(resp.content)
-        print(f"\tResult of {query_name} saved to {fname}")
+        return False
+
+    print(f"\tReturned code: {resp.status_code}")
+    fname = os.path.join(output_dir, f"{query_name}.{output_ext}")
+    with open(fname, "wb") as fout:
+        fout.write(resp.content)
+    print(f"\tResult of {query_name} saved to {fname}")
+    return True
          
 
-def execute_queries(endpoint, queries, output_dir, output_ext):
+def execute_queries(endpoint, queries, output_dir, output_ext, stop_on_error):
     if not queries:
         return
     for q in queries:
-        execute_query(endpoint, q, output_dir, output_ext)
+        ok = execute_query(endpoint, q, output_dir, output_ext)
+        if not ok and stop_on_error:
+            print("Stop on error enabled. Stopping.")
+            return
 
 def main():
     parser = argparse.ArgumentParser(
@@ -229,7 +236,10 @@ def main():
     parser.add_argument("-z", "--zoom", required=False, default=5)
     parser.add_argument("--endpoint", required=False, default="https://lz4.overpass-api.de/api/interpreter")
     parser.add_argument("--timeout", required=False, default=3600, type=int)
+    parser.add_argument("--maxsize", required=False, default=2000000000, type=int)
     parser.add_argument("--format", required=False, default="json")
+    parser.add_argument("--write_const_param_names", required=False, default=False)
+    parser.add_argument("-e", "--stop_on_error", required=False, default=False)
 
     args = parser.parse_args()
     config_dir = args.config_dir
@@ -238,16 +248,22 @@ def main():
     endpoint = args.endpoint
     timeout = args.timeout
     out_format = args.format
+    maxsize = args.maxsize
+    stop_on_error = args.stop_on_error
+    do_write_const_param_names = args.write_const_param_names
 
     os.makedirs(output_dir, exist_ok=True)
 
     config_names = get_configs(config_dir)
 
+    const_params = {"out_format": out_format, "timeout": timeout, "maxsize": maxsize}
+
     for config_name in config_names:
         tags = get_tags_by_config(config_dir, config_name)
-        query_texts = get_queries_from_tags(config_dir, config_name, out_format, timeout, tags, {"zoom" : lambda x: int(x) <= zoom})
-        execute_queries(endpoint, query_texts, output_dir, out_format)
+        query_texts = get_queries_from_tags(config_dir, config_name, const_params, tags, {"zoom" : lambda x: int(x) <= zoom}, do_write_const_param_names)
+        execute_queries(endpoint, query_texts, output_dir, out_format, stop_on_error)
     
+    print("Finished")
     
 if __name__ == "__main__":
     code = main()
